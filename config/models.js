8232be2ac7abdd9eeabf7e214a7b7dc05634ea84
shushmit_EQTribe <shushmit.yadav@eqtribe.com>
2020-04-11 21:39:04 +0530

    setting up mongo for project and installed sails-mongo hook for waterline orm

--- config/models.js
+++ config/models.js
@@ -53,7 +53,7 @@ module.exports.models = {
   *                                                                          *
   ***************************************************************************/
 
-  // migrate: 'alter',
+  migrate: 'safe',
 
 
   /***************************************************************************
@@ -69,9 +69,9 @@ module.exports.models = {
   ***************************************************************************/
 
   attributes: {
-    createdAt: { type: 'number', autoCreatedAt: true, },
-    updatedAt: { type: 'number', autoUpdatedAt: true, },
-    id: { type: 'number', autoIncrement: true, },
+    createdAt: { type: 'string', autoCreatedAt: true, columnType: 'datetime' },
+    updatedAt: { type: 'string', autoUpdatedAt: true, columnType: 'datetime' },
+    id: { type: 'string', columnName: '_id', autoIncrement: false },
     //--------------------------------------------------------------------------
     //  /\   Using MongoDB?
     //  ||   Replace `id` above with this instead:
