3ca60c68096c664706c1b7eaae9ac4edd8ac0968
shushmit-yadav <shushmit.yadav02@gmail.com>
2020-04-12 12:23:22 +0530

    API function to get all devices installed in system

--- api/controllers/DeviceController.js
+++ api/controllers/DeviceController.js
@@ -1,3 +1,4 @@
+'use strict';
 /**
  * DeviceController
  *
@@ -5,8 +6,36 @@
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
 
+var BaseCtrl = require('../base/Controller');
+
 module.exports = {
   
+    /**
+     * @author: shushmit yadav
+     * @description: this function will accept req and res 
+     *               It will find all installed devices and return to response
+     * @param {*} req 
+     * @param {*} res 
+     */
+    getAllDevices: function(req, res){
+        var requiredParamsError = BaseCtrl.checkRequiredParams(req, []);
+        if(requiredParamsError){
+            return res.badRequest(requiredParamsError);
+        } else {
+            
+            // find all devices
+            sails.models.device.find()
+            .then(function(devices){
+                return res.ok(devices);
+            })
+            .catch(function(err){
+                var errCode = err && err.code ? err.code : 500,
+                    errMessage = err && err.message ? err.message : err;
+                return res.status(errCode).send(errMessage);
+            });
+
+        }
+    }
 
 };
 
